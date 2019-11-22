e27f5133cc6818c258fbcc92ef8ac3ec61c1c79a
Giovanni Carignato <giovanni.carignato@gmail.com>
2019-11-21 23:24:04 -0300

    trabalhando na detalheProduto

--- phantomthieves-api/src/main/java/com/phantomthieves/api/controller/DetalheProdutoController.java
+++ phantomthieves-api/src/main/java/com/phantomthieves/api/controller/DetalheProdutoController.java
@@ -2,9 +2,12 @@ package com.phantomthieves.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,6 +22,7 @@ import com.phantomthieves.api.repository.ProdutoRepository;
 
 public class DetalheProdutoController {
 
+@Autowired
 private ProdutoRepository produtoRepository;
 	
 	@GetMapping
@@ -26,14 +30,14 @@ private ProdutoRepository produtoRepository;
 		ModelAndView resultado = new ModelAndView("index");
 		List<Produto> produto = produtoRepository.findAll();
 		resultado.addObject("produto", produto);
-
 		return resultado;
 	}
 
 	@GetMapping("/detalheProduto2")
-	
-	public ModelAndView detalheProduto2(Model model) {
+		public ModelAndView detalheProduto2(@ModelAttribute("cod_produto") Integer cod_produto) {
 		ModelAndView resultado = new ModelAndView("produtos/detalheProduto2");
+		List<Produto> produto = (List<Produto>) produtoRepository.findAllByCodigo(cod_produto);
+		resultado.addObject("produto", produto);
 		return resultado; 
 	}
 	
