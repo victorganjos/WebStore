7c46b73c49a00adc21023eae017a9614af836f53
Fabio Martins <fabiomartins1988@gmail.com>
2019-12-03 14:24:57 -0300

    Detalhe de produto funcionando, carrinho detalhe produto funcionando, ajuste Selecao de estado no cadastro do endereco

--- phantomthieves-api/src/main/java/com/phantomthieves/api/controller/DetalheProdutoController.java
+++ phantomthieves-api/src/main/java/com/phantomthieves/api/controller/DetalheProdutoController.java
@@ -1,40 +1,52 @@
 package com.phantomthieves.api.controller;
 
-import java.awt.Image;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.phantomthieves.api.model.Cliente;
-import com.phantomthieves.api.model.Endereco;
 import com.phantomthieves.api.model.Imagem;
 import com.phantomthieves.api.model.Produto;
+import com.phantomthieves.api.repository.ImagemRepository;
 import com.phantomthieves.api.repository.ProdutoRepository;
 
 @Controller
 @RequestMapping("/produtos")
 public class DetalheProdutoController {
-
+	
+	@Autowired
+	private ImagemRepository im;
+	
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
 
 	@PostMapping("/detalheProduto2")
-	public ModelAndView detalhe(@ModelAttribute("imagemNomeProduto")String nomeProduto) {
-		ModelAndView detalhes = new ModelAndView("produtos/detalheProduto2");
+	public ModelAndView detalhe(@ModelAttribute("imagemNomeProduto")String nomeProduto,
+			@ModelAttribute("imagemIdProduto")Integer id) {
+		ModelAndView resultado = new ModelAndView("produtos/detalheProduto2");
+		String local = "";
+		List<Imagem> imagem = im.findImg();
+		resultado.addObject("imagem", imagem);
 		Produto produto = new Produto();
-		produto = produtoRepository.findByNomeProduto(nomeProduto);
-				
-		detalhes.addObject("produto", produto);
+		for(Imagem i: imagem) {
 
-		return detalhes;
+			if(i.getCodProduto().getId() == id) {
+				produto = i.getCodProduto();
+				local = i.getLocalArquivo();
+			}
+		}
+		
+		System.out.println("teste detalhamento: " +nomeProduto+ " ID: " +id+ " Local da Imagem: " +local);
+
+		resultado.addObject("local", local);
+		resultado.addObject("produto", produto);
+		resultado.addObject("id", id);
+		return resultado;
 
 	}
 }
